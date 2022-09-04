8f9aa9951d64e850427d61e3a9504ccf22a12118
Xrathk <xrk1995@gmail.com>
2022-09-04 19:51:12 +0300

    Refactored program.cs for scalability purposes.

--- ApplicationLayer/Program.cs
+++ ApplicationLayer/Program.cs
@@ -1,80 +1,65 @@
 // Importing libraries
-using Microsoft.Extensions.FileProviders;
-using Microsoft.EntityFrameworkCore;
 using Serilog;
-using Serilog.Extensions.Logging;
-using DataAccessLayer.Data;
-using DataAccessLayer;
-using LogicLayer.Services;
-using DataAccessLayer.Repositories;
-using DataAccessLayer.Contacts;
+using ApplicationLayer.Startup;
 
 // Initializing app
 var builder = WebApplication.CreateBuilder(args);
 
-// Add logging (serilog)
+// Add logging 
 Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(builder.Configuration).CreateLogger();
-Log.Information("Setting up GetInfo application...");
-
-// Add services to the container.
-var connectionString = builder.Configuration.GetValue<string>("DbConnectionString");
-builder.Services.AddDbContext<GetInfoDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Logging.AddProvider(new SerilogLoggerProvider()); // Add serilog logging
 
 /// <summary>
-/// Adding custom services
+/// Add services to application service container.
 /// </summary>
+Log.Information("Setting up GetInfo services...");
+
+// Basic application services
+builder.Services.RegisterBasicServices();
+
+// Database context
+builder.RegisterApplicationDbContext();
+
 // Repos
-builder.Services.AddScoped<IRepositoryAppUser, RepositoryAppUser>();
-builder.Services.AddScoped<IRepositoryAppUserSession, RepositoryAppUserSession>();
-builder.Services.AddScoped<IRepositoryUserDetails, RepositoryUserDetails>();
-builder.Services.AddScoped<IRepositoryAppUserRequests, RepositoryAppUserRequests>();
-builder.Services.AddScoped<IRepositoryWeatherRequest, RepositoryWeatherRequest>();
-// BLL Services (+ optional DAL service)
-builder.Services.AddSingleton<ErrorCodes>(); // Add error codes
-builder.Services.AddTransient<AccountOperations>(); // Add account operations service
-builder.Services.AddTransient<WeatherApiOperations>(); // Add weather API operations service
-if (builder.Environment.IsDevelopment()) // Add one-time operations service (not for production environment)
+builder.Services.RegisterRepos();
+
+// BLL Services
+builder.Services.RegisterBusinessLogicServices();
+
+// Environment specific services
+if (builder.Environment.IsDevelopment())
 {
-    builder.Services.AddSingleton<OneTimeMethods>(); 
+    builder.Services.ConfigureDebugServices();
+}
+else if (builder.Environment.IsProduction())
+{
+    builder.Services.ConfigureProductionServices();
 }
 
-var app = builder.Build(); // Building
+// Build application
+var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+/// <summary>
+/// Configure application
+/// </summary>
+Log.Information("Setting up GetInfo application...");
 
-// Serving static files from wwwroot
-app.UseStaticFiles();
-// Serving static files from directory outside wwwroot
-app.UseStaticFiles(new StaticFileOptions
-{
-    FileProvider = new PhysicalFileProvider(
-           Path.Combine(builder.Environment.ContentRootPath, "Resources/Pictures")),
-    RequestPath = "/Pictures"
-});
+// HTTP request pipeline.
+app.ConfigureRequestPipeline();
+
+// Static files
+app.AddStaticFiles(builder);
 
 // Useful middleware
-app.UseRouting();
+app.AddMiddleware();
 
-app.MapControllers();
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
+// Setup ready - run application, exit if exception caught
 Log.Information("GetInfo setup successful. Starting app...");
 try
 {
-    app.Run(); // Running app
+    app.Run(); 
 }
 catch (Exception ex)
 {
     Log.Fatal("Application startup failed. Exception: {Ex}", ex); 
+    Environment.Exit(-1); 
 }
